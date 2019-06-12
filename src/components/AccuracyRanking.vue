222cdb3298c851ee135a6c21a2820199021dcab0
Lan Vu <vungoclancfc@gmail.com>
2019-06-12 00:10:14 -0500

    Add image and view-source link to AccuracyRanking

--- src/components/AccuracyRanking.vue
+++ src/components/AccuracyRanking.vue
@@ -5,7 +5,18 @@
         <h5>Accuracy Ranking</h5>
       </li>
       <li v-for="source in sources" :key="source.name" class="collection-item">
-        <span class="title"> {{ source.name }} </span>
+        <v-avatar size="32px" v-if="source.id">
+          <img class="img-circle mb-1" :src="getImgUrl(source.id)" />
+        </v-avatar>
+        <router-link
+          class="title"
+          :to="{
+            name: 'view-source',
+            params: { sourceName: source.name }
+          }"
+        >
+          {{ source.name }}</router-link
+        >
         <span
           class="badge new"
           data-badge-caption="%"
@@ -47,6 +58,12 @@ export default {
           this.sources.push(data);
         });
       });
+  },
+  methods: {
+    getImgUrl(pic) {
+      if (pic) return require('../assets/images/' + pic + '.png');
+      else return null;
+    }
   }
 };
 </script>
